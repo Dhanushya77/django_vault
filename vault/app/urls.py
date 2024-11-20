b795c4b701e7c32dcb09e63d6c8fb38845a4eed7
Dhanushya V S <75406996+Dhanushya77@users.noreply.github.com>
2024-11-21 04:33:05 +0530

    vault- user register , add and view files

--- vault/app/urls.py
+++ vault/app/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.user_login),
-    # path('user_logout'.views.user_logout),
+    path('user_logout',views.user_logout),
     path('register',views.register),
     path('vault',views.vault),
+    path('add_files/<id>',views.add_files),
+    path('delete_file',views.delete_file),
+    
+    
 ]
--- vault/app/views.py
+++ vault/app/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.models import User
+from.models import *
+
 # Create your views here.
+
+
+# ------------------user login-------------------------
+
+
 def user_login(req):
     if req.method == 'POST':
         uname = req.POST['uname']
@@ -13,11 +20,20 @@ def user_login(req):
             return redirect(vault)
         else:
             messages.warning(req,'Invalid username or password')
-            return redirect(e_com_login)
+            return redirect(user_login)
     return render(req,'login.html')
-# def user_logout(req):
-#     logout(req)
-#     return redirect(user_login)
+
+
+# --------------------user logout---------------------------
+
+
+def user_logout(req):
+    logout(req)
+    return redirect(user_login)
+
+
+# -------------------user registration------------------------
+
 
 def register(req):
     if req.method=='POST':
@@ -30,11 +46,38 @@ def register(req):
             return redirect(user_login)
         except:
             messages.warning(req,'Email already exist')
-            return redirect(user_login)
+            return redirect(register)
     else:
         return render(req,'register.html')
 
+
+# ---------------------vault-------------------------
+
+
 def vault(req):
-    return render(req,'vault.html')
+    files=File.objects.filter(user=req.user)
+    return render(req,'vault.html',{'files':files})
+
+
+def add_files(req,id):
+    if req.method=='POST':
+        user = User.objects.get(pk=id)
+        name = req.POST['name']
+        file = req.FILES['file']
+        data = File.objects.create(user=user,name=name,file=file)
+        data.save()
+        return redirect(vault)
+    return render(req,'file.html')
+
+def delete_file(req):
+    # if req.method=='POST':
+    #     name=req.POST.get(name)
+    #     print(name)
+    #     file=File.objects.get(name=name,user=req.user)
+    #     file.delete()
+    return redirect(vault)
+    
+    
+        
 
     
